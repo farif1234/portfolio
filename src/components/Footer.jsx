9b3992c151eefc2e72f127336be3ef5cc2e73f6d
Faihaan Arif <farif1234@gmail.com>
2022-11-17 21:11:40 -0500

    center footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <span class="block text-sm text-gray-500 sm:text-center bg-gray-800 ">
+        <span class="block text-sm text-gray-500 text-center bg-gray-800 ">
             © 2022 Faihaan Arif
         </span>
     );
